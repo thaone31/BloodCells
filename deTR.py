c2e3c8b3dcb0c547003eaeb75205f18a21c4278f
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-02-03 23:09:57 +0700

    Update deTR.py

--- deTR.py
+++ deTR.py
@@ -75,10 +75,10 @@ seed_everything(seed)
 # g.set_xticklabels(encoder.classes_);
 
 
-# df_split = df[['image_id']].copy()
-# # df_split = pd.concat([df_split,pd.get_dummies(df.labels).astype('int32')],axis=1)
-# df_split['bbox_count'] = 1
-# df_split = df_split.groupby('image_id').sum()
+df_split = df[['image_id']].copy()
+# df_split = pd.concat([df_split,pd.get_dummies(df.labels).astype('int32')],axis=1)
+df_split['bbox_count'] = 1
+df_split = df_split.groupby('image_id').sum()
 
 # g = plt.figure(figsize=(15,4))
 # g = sns.countplot(x='bbox_count',data=df_split)
